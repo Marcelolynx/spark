fc44c4a0da3899d8707f323385308a13eb1d3a20
Marcelo Oliveira <marcelolynx@Mac-mini-de-Marcelo.local>
2018-03-31 08:53:29 -0400

    ajustando login

--- src/main/java/br/com/kluster/api/tools/GeradorSenha.java
+++ src/main/java/br/com/kluster/api/tools/GeradorSenha.java
@@ -6,6 +6,6 @@ public class GeradorSenha {
 
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println(encoder.encode("1234"));
+        System.out.println(encoder.encode("mco02jgp"));
     }
 }
