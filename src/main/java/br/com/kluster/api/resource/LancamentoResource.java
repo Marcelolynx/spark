13a959152db5b52db2c331454a771e92d55c5aee
Marcelo Oliveira <marcelolynx@Mac-mini-de-Marcelo.local>
2018-04-14 19:10:28 -0400

    adicionando as permissoes

--- src/main/java/br/com/kluster/api/resource/LancamentoResource.java
+++ src/main/java/br/com/kluster/api/resource/LancamentoResource.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -42,19 +43,19 @@ public class LancamentoResource {
 
 
     @GetMapping
-   // @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')")
     public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         return lancamentoRepository.filtrar(lancamentoFilter, pageable);
     }
 
     @GetMapping(params = "resumo")
-   // @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
     public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable) {
         return lancamentoRepository.resumir(lancamentoFilter, pageable);
     }
 
     @PostMapping
-   // @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO')")
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO')")
     public ResponseEntity<Lancamento> save(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
         Lancamento lancamentoSave = lancamentoService.salvar(lancamento);
 
@@ -64,7 +65,7 @@ public class LancamentoResource {
     }
 
     @GetMapping("/{id}")
-    //@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO')")
     public ResponseEntity<Lancamento> findById(@PathVariable Long id) {
         Lancamento lancamento = lancamentoRepository.findOne(id);
         return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
@@ -72,7 +73,7 @@ public class LancamentoResource {
 
     @DeleteMapping("/{codigo}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-   // @PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO')")
+    @PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO')")
     public void remover(@PathVariable Long codigo) {
         lancamentoRepository.delete(codigo);
     }
